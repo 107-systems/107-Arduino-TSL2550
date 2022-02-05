581934585866c45473ea75e1edaa521abb13e41f
Bernhard Mayer <bernhard@generationmake.de>
2022-02-05 03:01:10 +0100

    added basic example

---
+++ examples/TSL2550-Basic/TSL2550-Basic.ino
@@ -0,0 +1,86 @@
+/**
+ * @brief Basic example demonstrating usage of 107-Arduino-TSL2550 library.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <Wire.h>
+
+#include <107-Arduino-TSL2550.h>
+
+/**************************************************************************************
+ * FUNCTION DECLARATION
+ **************************************************************************************/
+
+void i2c_generic_write(uint8_t const i2c_slave_addr, uint8_t const reg_addr, uint8_t const * buf, uint8_t const num_bytes);
+void i2c_generic_read (uint8_t const i2c_slave_addr, uint8_t const reg_addr, uint8_t       * buf, uint8_t const num_bytes);
+
+/**************************************************************************************
+ * GLOBAL VARIABLES
+ **************************************************************************************/
+
+ArduinoTSL2550 tsl2550(i2c_generic_write,
+                       i2c_generic_read,
+//                       delay,
+                       TSL2550::DEFAULT_I2C_ADDR);
+
+/**************************************************************************************
+ * SETUP/LOOP
+ **************************************************************************************/
+
+void setup()
+{
+  Serial.begin(9600);
+  while(!Serial) { }
+
+  /* Setup Wire access */
+  Wire.begin();
+
+  /* Configure TSL2550 with Extended Range */
+  if (!tsl2550.begin(true))
+  {
+    Serial.print("ArduinoTSL2550::begin(...) failed, error code ");
+    Serial.print((int)tsl2550.error());
+    for(;;) { }
+  }
+
+  Serial.println("TSL2550 OK");
+}
+
+void loop()
+{
+  float light_level=tsl2550.get_lux();
+  Serial.print("Light level = ");
+  Serial.print(light_level);
+  Serial.println(" lux");
+
+  delay(500);
+}
+
+/**************************************************************************************
+ * FUNCTION DEFINITION
+ **************************************************************************************/
+
+void i2c_generic_write(uint8_t const i2c_slave_addr, uint8_t const reg_addr, uint8_t const * buf, uint8_t const num_bytes)
+{
+  Wire.beginTransmission(i2c_slave_addr);
+  Wire.write(reg_addr);
+  for(uint8_t bytes_written = 0; bytes_written < num_bytes; bytes_written++) {
+    Wire.write(buf[bytes_written]);
+  }
+  Wire.endTransmission();
+}
+
+void i2c_generic_read(uint8_t const i2c_slave_addr, uint8_t const reg_addr, uint8_t * buf, uint8_t const num_bytes)
+{
+  Wire.beginTransmission(i2c_slave_addr);
+  Wire.write(reg_addr);
+  Wire.endTransmission();
+
+  Wire.requestFrom(i2c_slave_addr, num_bytes);
+  for(uint8_t bytes_read = 0; (bytes_read < num_bytes) && Wire.available(); bytes_read++) {
+    buf[bytes_read] = Wire.read();
+  }
+}
